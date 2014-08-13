144a74be5b4f732a5d53d476cf35e307589eadef
轩与 <wangwei.wang@alipay.com>
2014-08-13 19:13:40 +0800

    增加amw dest的gulp plugin方法

--- amw-dest-gulp/index.js
+++ amw-dest-gulp/index.js
@@ -1,29 +1,18 @@
 var es = require('event-stream'),
-	vfs = require('vinyl-fs'),
+	dest = require('./lib/am-fs'),
 	fs = require('fs'),
 	path = require('path');
 
 // Plugin function
-function amDest(options) {
-	if (!options) options = {};
+function amDest(path) {
+	path = path || "./";
 
-	function buildDest(file, callback) {
-		//将文件复制到新的文件夹
-		var pakageData = JSON.parse(fs.readFileSync('package.json'));
-		console.log(pakageData);
+	//将文件复制到新的文件夹
+	var pakageData = JSON.parse(fs.readFileSync('package.json'));
 
-		if (file.isNull()) return callback(null, file);
+	var stream = dest(path);
 
-		if (file.isStream()) {
-			return callback(null, file);
-		}
-
-		var newFile = file.clone();
-		newFile.contents = new Buffer(newFile.contents);
-		callback(null, newFile);
-	}
-
-	return es.map(buildDest);
+	return stream;
 }
 
 // Export the plugin main function
